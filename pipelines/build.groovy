30cc5c833437fb9c2abceb7e5262e6126e97b536
stefanbesler <11271989+stefanbesler@users.noreply.github.com>
2021-07-26 21:57:05 +0200

    Update build.groovy

--- pipelines/build.groovy
+++ pipelines/build.groovy
@@ -3,7 +3,6 @@
 @Library(['jenkinslibs']) _
 pipeline_build(remote_base: 'git@github.com:stefanbesler',
                git_user: 'git_iabot_ssh',
-               git_accesstoken: 'git_stefanbesler_accesstoken',
                default_variants: '',
                default_create_tag: false,
                default_create_commit: false,
